3329f311739a471c8ede5f04ea359da5aa9c0fa5
Uriel SAUTRON <uriel.sautron@gmail.com>
2022-06-22 17:38:09 +0200

    Create SophiaIcon.jsx and delete svg

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,17 +1,3 @@
-import {
-  CornucopiaIcon,
-  BasketIcon,
-  CheckBoxIcon,
-  SophiaIcon,
-} from "../assets/svgIcon";
-
 export default function Home() {
-  return (
-    <header className="App-header">
-      <CornucopiaIcon />
-      <BasketIcon />
-      <CheckBoxIcon />
-      <SophiaIcon />
-    </header>
-  );
+  return <header className="App-header" />;
 }
