bfa910b6d3829e54db1763da1fac7388d205eeba
Uriel SAUTRON <uriel.sautron@gmail.com>
2022-06-26 19:20:13 +0200

    Transforms ArrowIcon, PlusIcon, HomeIcon, UserIcon to React Components

---
+++ frontend/src/assets/iconsCard/index.js
@@ -0,0 +1,2 @@
+export { PlusIcon } from "./PlusIcon";
+export { ArrowIcon } from "./ArrowIcon";
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Button, TutoCardList } from "@components";
+import { PlusIcon, ArrowIcon } from "@assets/iconsCard";
 import "./Home.scss";
 
 const Home = () => {
   return (
     <div className="home-container">
       <TutoCardList />
+      <PlusIcon />
+      <ArrowIcon />
       <Link to="/go-sophia">
         <Button buttonSize="large" buttonStyle="dark">
           Go SOPHIA !
