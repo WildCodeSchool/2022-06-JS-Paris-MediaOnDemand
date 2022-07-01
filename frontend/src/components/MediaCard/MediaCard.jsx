187aa02eb87463ecfd0a7d26e26dbc75ef2665f8
Yohh <durandyohan@zaclys.net>
2022-07-01 14:19:25 +0200

    change gradient in MediaCard.scss, add HeartIcon.jsx and pull it in index.js,  add HeartIcon.jsx in MediaCard.scss

--- frontend/src/components/MediaCard/MediaCard.jsx
+++ frontend/src/components/MediaCard/MediaCard.jsx
@@ -2,31 +2,29 @@ import React from "react";
 import "../../theme.scss";
 import "./MediaCard.scss";
 import "@components/Button/Button.scss";
-import { PlusIcon } from "@assets/iconsCard";
+import { PlusIcon, HeartIcon } from "@assets/iconsCard";
 import { Button } from "@components";
 import { Link } from "react-router-dom";
-import heartIcon from "../../assets/heartIcon.svg";
 
-export const MediaCard =
-  (/* { imgUrl, title, category, current, total } */) => {
-    return (
-      <div className="mediaCard">
-        <div className="imageMediaCard">
-          <div className="iconUpMediaCard">
-            <div className="counter">
-              <span>*/*</span>
-            </div>
-            <img src={heartIcon} alt="add" />
+export const MediaCard = () => {
+  return (
+    <div className="mediaCard">
+      <div className="imageMediaCard">
+        <div className="iconUpMediaCard">
+          <div className="counter">
+            <span>0/0</span>
           </div>
-          <PlusIcon />
+          <HeartIcon />
         </div>
-        <div className="titleMediaCard">title</div>
-        <div className="categoriesMediaCard">categories</div>
-        <Link to="/">
-          <Button buttonSize="small" buttonStyle="dark">
-            infos
-          </Button>
-        </Link>
+        <PlusIcon />
       </div>
-    );
-  };
+      <div className="titleMediaCard">title</div>
+      <div className="categoriesMediaCard">categories</div>
+      <Link to="/">
+        <Button buttonSize="small" buttonStyle="dark">
+          infos
+        </Button>
+      </Link>
+    </div>
+  );
+};
