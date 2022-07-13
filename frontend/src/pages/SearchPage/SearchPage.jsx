3de705ab42b04701c6ddb2f2460361d557e917b8
Uriel SAUTRON <uriel.sautron@gmail.com>
2022-07-13 16:03:55 +0200

    Refacto SelectCat for input, Create true select option for search media and connect with fetch movie, add go button for fetch with select

--- frontend/src/pages/SearchPage/SearchPage.jsx
+++ frontend/src/pages/SearchPage/SearchPage.jsx
@@ -20,6 +20,99 @@ import {
   useMediaSelectedContext,
 } from "@context";
 
+const bookGenres = [
+  { value: "fantastic", text: "Fantastic" },
+  { value: "family", text: "Famille" },
+  { value: "action", text: "Action" },
+  { value: "horror", text: "Horreur" },
+  { value: "drama", text: "Drame" },
+  { value: "polar", text: "Polor" },
+  { value: "thriller", text: "Thriller" },
+  { value: "post-apocalyptic", text: "Post-Apocalyptique" },
+  { value: "comedy", text: "Comedie" },
+  { value: "adventure", text: "Aventure" },
+  { value: "historic", text: "Historique" },
+];
+
+const movieGenres = [
+  {
+    id: 28,
+    name: "Action",
+  },
+  {
+    id: 12,
+    name: "Adventure",
+  },
+  {
+    id: 16,
+    name: "Animation",
+  },
+  {
+    id: 35,
+    name: "Comedy",
+  },
+  {
+    id: 80,
+    name: "Crime",
+  },
+  {
+    id: 99,
+    name: "Documentary",
+  },
+  {
+    id: 18,
+    name: "Drama",
+  },
+  {
+    id: 10751,
+    name: "Family",
+  },
+  {
+    id: 14,
+    name: "Fantasy",
+  },
+  {
+    id: 36,
+    name: "History",
+  },
+  {
+    id: 27,
+    name: "Horror",
+  },
+  {
+    id: 10402,
+    name: "Music",
+  },
+  {
+    id: 9648,
+    name: "Mystery",
+  },
+  {
+    id: 10749,
+    name: "Romance",
+  },
+  {
+    id: 878,
+    name: "Science Fiction",
+  },
+  {
+    id: 10770,
+    name: "TV Movie",
+  },
+  {
+    id: 53,
+    name: "Thriller",
+  },
+  {
+    id: 10752,
+    name: "War",
+  },
+  {
+    id: 37,
+    name: "Western",
+  },
+];
+
 const mediaCatSearch = [
   {
     mediaName: "Film",
@@ -31,9 +124,31 @@ const mediaCatSearch = [
         selectLabel: "Année",
         defaultOption: "Choisis une Année",
         selectOptions: [
-          { value: 1990, text: "1990" },
-          { value: 2000, text: "2000" },
-          { value: 2010, text: "2010" },
+          { value: "&primary_release_date.lte=1979-12-31", text: "Avant 1980" },
+          {
+            value:
+              "&primary_release_date.gte=1980-01-01&primary_release_date.lte=1989-12-31",
+            text: "Entre 1980 et 1990",
+          },
+          {
+            value:
+              "&primary_release_date.gte=1990-01-01&primary_release_date.lte=1999-12-31",
+            text: "Entre 1990 et 2000",
+          },
+          {
+            value:
+              "&primary_release_date.gte=2000-01-01&primary_release_date.lte=2009-12-31",
+            text: "Entre 2000 et 2010",
+          },
+          {
+            value:
+              "&primary_release_date.gte=2010-01-01&primary_release_date.lte=2019-12-31",
+            text: "Entre 2010 et 2020",
+          },
+          {
+            value: "&primary_release_date.gte=2020-01-01",
+            text: "A partir de 2020",
+          },
         ],
       },
       {
@@ -41,144 +156,32 @@ const mediaCatSearch = [
         selectId: "genre-select",
         selectLabel: "Genre",
         defaultOption: "Choisis un Genre",
-        selectOptions: [
-          { value: "fiction", text: "Fiction" },
-          { value: "action", text: "Action" },
-          { value: "romance", text: "Romance" },
-        ],
-      },
-      {
-        uuidSelect: uuidv4(),
-        selectId: "pay-select",
-        selectLabel: "Pays",
-        defaultOption: "Choisis un Pays",
-        selectOptions: [
-          { value: "france", text: "France" },
-          { value: "espagne", text: "Espagne" },
-          { value: "japon", text: "Japon" },
-        ],
+        selectOptions: movieGenres.map((genre) => {
+          return { value: `&with_genres=${genre.id}`, text: genre.name };
+        }),
       },
     ],
   },
-  {
-    mediaName: "Musique",
-    mediaCatName: "music",
-    mediaSelectList: [
-      {
-        uuidSelect: uuidv4(),
-        selectId: "annee-select",
-        selectLabel: "Année",
-        defaultOption: "Choisis une Année",
-        selectOptions: [
-          { value: 1990, text: "1990" },
-          { value: 2000, text: "2000" },
-          { value: 2010, text: "2010" },
-        ],
-      },
-      {
-        uuidSelect: uuidv4(),
-        selectId: "genre-select",
-        selectLabel: "Genre",
-        defaultOption: "Choisis un Genre",
-        selectOptions: [
-          { value: "fiction", text: "Fiction" },
-          { value: "action", text: "Action" },
-          { value: "romance", text: "Romance" },
-        ],
-      },
-      {
-        uuidSelect: uuidv4(),
-        selectId: "pay-select",
-        selectLabel: "Pays",
-        defaultOption: "Choisis un Pays",
-        selectOptions: [
-          { value: "france", text: "France" },
-          { value: "espagne", text: "Espagne" },
-          { value: "japon", text: "Japon" },
-        ],
-      },
-      {
-        uuidSelect: uuidv4(),
-        selectId: "genre-select",
-        selectLabel: "Genre",
-        defaultOption: "Choisis un Genre",
-        selectOptions: [
-          { value: "fiction", text: "Fiction" },
-          { value: "action", text: "Action" },
-          { value: "romance", text: "Romance" },
-        ],
-      },
-      {
-        uuidSelect: uuidv4(),
-        selectId: "pay-select",
-        selectLabel: "Pays",
-        defaultOption: "Choisis un Pays",
-        selectOptions: [
-          { value: "france", text: "France" },
-          { value: "espagne", text: "Espagne" },
-          { value: "japon", text: "Japon" },
-        ],
-      },
-    ],
-  },
+
+  // ?Book
   {
     mediaName: "Livre",
     mediaCatName: "book",
     mediaSelectList: [
       {
         uuidSelect: uuidv4(),
-        selectId: "annee-select",
-        selectLabel: "Année",
-        defaultOption: "Choisis une Année",
-        selectOptions: [
-          { value: 1990, text: "1990" },
-          { value: 2000, text: "2000" },
-          { value: 2010, text: "2010" },
-        ],
-      },
-      {
-        uuidSelect: uuidv4(),
         selectId: "genre-select",
         selectLabel: "Genre",
         defaultOption: "Choisis un Genre",
-        selectOptions: [
-          { value: "fiction", text: "Fiction" },
-          { value: "action", text: "Action" },
-          { value: "romance", text: "Romance" },
-        ],
+        selectOptions: bookGenres.map((genre) => {
+          return { value: genre.value, text: genre.text };
+        }),
       },
       {
         uuidSelect: uuidv4(),
-        selectId: "pay-select",
-        selectLabel: "Pays",
-        defaultOption: "Choisis un Pays",
-        selectOptions: [
-          { value: "france", text: "France" },
-          { value: "espagne", text: "Espagne" },
-          { value: "japon", text: "Japon" },
-        ],
-      },
-      {
-        uuidSelect: uuidv4(),
-        selectId: "genre-select",
-        selectLabel: "Genre",
-        defaultOption: "Choisis un Genre",
-        selectOptions: [
-          { value: "fiction", text: "Fiction" },
-          { value: "action", text: "Action" },
-          { value: "romance", text: "Romance" },
-        ],
-      },
-      {
-        uuidSelect: uuidv4(),
-        selectId: "pay-select",
-        selectLabel: "Pays",
-        defaultOption: "Choisis un Pays",
-        selectOptions: [
-          { value: "france", text: "France" },
-          { value: "espagne", text: "Espagne" },
-          { value: "japon", text: "Japon" },
-        ],
+        selectId: "author-select",
+        selectLabel: "Auteur",
+        defaultOption: "Choisis un Auteur",
       },
     ],
   },
@@ -188,7 +191,7 @@ export const SearchPage = () => {
   const [searchInputValue, setSearchInputValue] = useState("");
   const [isCatOpen, setIsCatOpen] = useState(false);
 
-  const [setFiltersSelected] = useState({
+  const [filtersSelected, setFiltersSelected] = useState({
     book: [],
     music: [],
     movie: [],
@@ -203,6 +206,19 @@ export const SearchPage = () => {
   const handleFetchMovie = () => {
     const option = searchInputValue ? "search" : "discover";
     const searchQuery = searchInputValue ? `&query=${searchInputValue}` : null;
+    if (isMediaSelected(mediasSelected, "movie")) {
+      fetchMovies(option, [searchQuery], setMovies);
+    } else {
+      setMovies([]);
+    }
+  };
+
+  const handleFetchMovieSlect = () => {
+    const option = "discover";
+    const searchQuery = filtersSelected.movie.reduce(
+      (acc, select) => acc + select.value,
+      ""
+    );
     if (isMediaSelected(mediasSelected, "movie")) {
       fetchMovies(option, [searchQuery], setMovies);
     } else {
@@ -234,6 +250,12 @@ export const SearchPage = () => {
     navigate("../display", { replace: true });
   };
 
+  const handleFetchMediaSelect = (e) => {
+    e.preventDefault();
+    handleFetchMovieSlect();
+    navigate("../display", { replace: true });
+  };
+
   return (
     <div className="search-page">
       <div className="search-page__section">
@@ -252,20 +274,30 @@ export const SearchPage = () => {
             Catégories
           </Button>
         </div>
-        {isCatOpen && (
-          <div className="search-page__select-list">
-            {mediaCatSearch.map(
-              (mediaCat) =>
-                mediasSelected.includes(mediaCat.mediaCatName) && (
-                  <SelectCatList
-                    key={mediaCat.mediaName}
-                    mediaCat={mediaCat}
-                    setFiltersSelected={setFiltersSelected}
-                  />
-                )
-            )}
-          </div>
-        )}
+        {isCatOpen &&
+          (isMediaSelected(mediasSelected, "movie") ||
+            isMediaSelected(mediasSelected, "book")) && (
+            <div className="search-page__select-list">
+              {mediaCatSearch.map(
+                (mediaCat) =>
+                  mediasSelected.includes(mediaCat.mediaCatName) && (
+                    <SelectCatList
+                      key={mediaCat.mediaName}
+                      mediaCat={mediaCat}
+                      setFiltersSelected={setFiltersSelected}
+                    />
+                  )
+              )}
+
+              <Button
+                buttonSize="large"
+                buttonStyle="dark"
+                onClick={(e) => handleFetchMediaSelect(e)}
+              >
+                Go !
+              </Button>
+            </div>
+          )}
       </div>
       <TabBar />
     </div>
