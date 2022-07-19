8c38495e53c17e150142c7aa7b6aa54530bf45be
Yohh <durandyohan@zaclys.net>
2022-07-19 11:30:06 +0200

    download progress in progress

--- frontend/src/components/UserPurchase/UserPurchase.jsx
+++ frontend/src/components/UserPurchase/UserPurchase.jsx
@@ -1,17 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../../theme.scss";
 import "./UserPurchase.scss";
 import "@components/Button/Button.scss";
 import { DownloadIcon } from "@assets/svgIcon";
 
 export const UserPurchase = ({ title, image }) => {
-  // const handleClick = () => {};
+  const [download, setDownload] = useState(false);
+  // let width = 0;
+  const [barWidth, setBarWidth] = useState(0);
+  const [message, setMessage] = useState("");
 
-  return (
+  const handleClick = () => {
+    setDownload(!download);
+  };
+
+  useEffect(() => {
+    let width = 0;
+    if (download) {
+      setInterval(() => {
+        width += 5;
+        setBarWidth(width);
+      }, 100);
+    }
+    return () => {};
+  }, [download]);
+
+  if (barWidth === 100) {
+    clearInterval(setInterval);
+    setMessage("téléchargement terminé!");
+  }
+
+  return !download ? (
     <div className="purchaseCard">
       <img src={image} className="purchaseCard__image" alt="" />
       <h3 className="purchaseCard__title">{title}</h3>
-      <DownloadIcon />
+      <DownloadIcon onClick={() => handleClick()} />
+    </div>
+  ) : (
+    <div className="downloadCard">
+      <div className="downloadBar">
+        <div className="downloadProgress" style={{ width: `${barWidth}%` }}>
+          {`${barWidth}%`}
+        </div>
+      </div>
+      <div>{message}</div>
     </div>
   );
 };
