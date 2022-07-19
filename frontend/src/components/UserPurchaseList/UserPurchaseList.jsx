ee17d9d5c9cfbf0049d0d6851f7a8ad47151e542
Uriel SAUTRON <uriel.sautron@gmail.com>
2022-07-19 21:10:19 +0200

    Fix TabUser

--- frontend/src/components/UserPurchaseList/UserPurchaseList.jsx
+++ frontend/src/components/UserPurchaseList/UserPurchaseList.jsx
@@ -11,14 +11,17 @@ export const UserPurchaseList = () => {
     desktop: {
       breakpoint: { max: 4000, min: 1024 },
       items: 3,
+      slidesToSlide: 3,
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
       items: 2,
+      slidesToSlide: 2,
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },
       items: 1,
+      slidesToSlide: 1,
     },
   };
 
