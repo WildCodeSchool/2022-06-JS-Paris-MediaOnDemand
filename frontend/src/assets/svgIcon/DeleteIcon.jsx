7a1711a8b0e4b4956500e49f87166e9d3e175e0b
Rico-Developpeur <eric.vieira@sncf.fr>
2022-07-20 10:40:52 +0200

    add animated icons; modification SearchInput.scss

--- frontend/src/assets/svgIcon/DeleteIcon.jsx
+++ frontend/src/assets/svgIcon/DeleteIcon.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import "../AnimatedIcon.scss";
 
 export const DeleteIcon = (props) => {
   return (
     <svg
+      className="animatedIcon"
       xmlns="http://www.w3.org/2000/svg"
       width="40"
       height="38.667"
