ee1f1d988d1147424588f154fdf658c9911277c8
Rico-Developpeur <eric.vieira@sncf.fr>
2022-07-21 10:56:20 +0200

    add navigate logo and cursor

--- frontend/src/components/TabBar/TabBar.jsx
+++ frontend/src/components/TabBar/TabBar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { UserIcon, HomeIcon } from "@assets/iconsTabBar";
 import { useThemeContext } from "@context";
 import { SophiaIcon, SophiaIconVertical } from "../../assets/svgIcon";
@@ -8,6 +8,7 @@ import "./TabBar.scss";
 export const TabBar = () => {
   const { theme } = useThemeContext();
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const navigate = useNavigate();
 
   let colorIcon = "#ccc591";
   if (theme === "theme--movie") colorIcon = "#80a0ff";
@@ -17,15 +18,24 @@ export const TabBar = () => {
   window.addEventListener("resize", () => {
     setWindowWidth(window.innerWidth);
   });
+
+  const handleNavigate = () => navigate("/");
+
   return (
     <div className="nav-bar">
       <Link to="/go-sophia" className="tabBarIcon">
         <HomeIcon colorIcon={colorIcon} />
       </Link>
       {windowWidth < 768 || windowWidth > 1439 ? (
-        <SophiaIcon className="logoIcon" />
+        <SophiaIcon
+          className="logoIcon cursor"
+          onClick={() => handleNavigate()}
+        />
       ) : (
-        <SophiaIconVertical />
+        <SophiaIconVertical
+          classeName="cursor"
+          onClick={() => handleNavigate()}
+        />
       )}
       <Link to="/profile" className="tabBarIcon">
         <UserIcon colorIcon={colorIcon} />
